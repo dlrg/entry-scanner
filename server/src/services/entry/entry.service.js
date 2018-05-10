6b8d30ade4946f845fe6270a4aa6d4258eac6263
Torge Jürgensen <torge.juergensen@codeanker.de>
2018-05-10 22:53:09 +0200

    switch to nedb

--- server/src/services/entry/entry.service.js
+++ server/src/services/entry/entry.service.js
@@ -1,20 +1,23 @@
 // Initializes the `entry` service on path `/entry`
-const createService = require('feathers-mongodb');
+const createService = require('feathers-nedb');
+const createModel = require('../../models/entry.model');
 const hooks = require('./entry.hooks');
 
 module.exports = function (app) {
-  const mongoClient = app.get('mongoClient');
-  const options = { };
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    name: 'entry',
+    Model,
+    paginate
+  };
 
   // Initialize our service with any options it requires
   app.use('/entry', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('entry');
-
-  mongoClient.then(db => {
-    service.Model = db.collection('entry');
-  });
 
   service.hooks(hooks);
 };
