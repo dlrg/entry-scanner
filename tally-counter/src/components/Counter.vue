03bbd2e640102d9069cc98c6421fe46160369a28
Barne Jürgensen <barne.juergensen@codeanker.de>
2018-05-11 20:39:18 +0200

    add tally counter

---
+++ tally-counter/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="counter">
+    <h1 class="counter__value">{{value}}</h1>
+    <p class="counter__name">{{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Number,
+    name: String
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tally-counter/src/views/TallyCounter.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <counter :value="ownCount" name="Selber"/>
+  </main>
+</template>
+
+<script>
+import Counter from '../components/Counter'
+
+export default {
+  components: {
+    Counter
+  },
+  data () {
+    return {
+      ownCount: 20,
+      globalCount: 300
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
